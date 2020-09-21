b7f2ae6a3c3bb26d1b669e77c4b61d980c78bd01
Cezary Jaworski <cezjaw@st.amu.edu.pl>
2020-09-21 22:31:31 +0200

    Added solutions for second task

--- zestaw_2.py
+++ zestaw_2.py
@@ -1,9 +1,9 @@
-#importing csv, pandas and matplotlib libraries and setting options
+#importing csv, pandas and sklearn libraries and setting options
 import pandas as pd
-import matplotlib.pyplot as plt
+import sklearn.preprocessing as skl
 pd.set_option('display.width',None)
 pd.set_option('display.max_colwidth', None)
-df = pd.read_csv("J:/Studia/Uczenie_maszynowe/survey_results_public.csv",usecols=["Respondent", "OrgSize", "YearsCode", "YearsCodePro", "LastHireDate","ConvertedComp"], index_col="Respondent")
+df = pd.read_csv("J:/Studia/Uczenie_maszynowe/survey_results_public.csv",usecols=["Respondent", "OrgSize", "YearsCode", "YearsCodePro", "LastHireDate","ConvertedComp","Student","UndergradMajor"], index_col="Respondent")
 #replacing string values and converting to floats
 df.replace(to_replace='Less than 1 year', value='0', inplace=True)
 df.replace(to_replace='More than 50 years', value='51', inplace=True)
@@ -23,13 +23,25 @@ df.replace(to_replace='500 to 999 employees', value='500', inplace=True)
 df.replace(to_replace='1,000 to 4,999 employees', value='1000', inplace=True)
 df.replace(to_replace='5,000 to 9,999 employees', value='5000', inplace=True)
 df.replace(to_replace='10,000 or more employees', value='10000', inplace=True)
+df.replace(to_replace='No', value='0', inplace=True)
+df.replace(to_replace='Yes, full-time', value='1', inplace=True)
+df.replace(to_replace='Yes, part-time', value='1', inplace=True)
+df.dropna(inplace=True)
 df['OrgSize'] = df['OrgSize'].astype(float)
 df['YearsCode'] = df['YearsCode'].astype(float)
 df['YearsCodePro'] = df['YearsCodePro'].astype(float)
 df['LastHireDate'] = df['LastHireDate'].astype(float)
+df['Student'] = df['Student'].astype(int)
 #seeking for correlations
-print(df.corr())
+df.corr()
 #setting variables
 x1=df['OrgSize']
 x2=df['YearsCodePro']
 y=df['ConvertedComp']
+#creating a binarize lebeled numpy arraies with usage of column UndergradMajor
+label_bin = skl.LabelBinarizer()
+transformed_label_bin = label_bin.fit_transform(df['UndergradMajor'])
+#tranfosrming into DataFrame
+df_label_bin= pd.DataFrame(transformed_label_bin, columns=label_bin.classes_)
+#joining to primary DataFrame
+df = df.join(df_label_bin)
