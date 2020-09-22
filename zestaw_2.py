226ec807e4e55906a1524326d48c0c863366980a
Cezary Jaworski <cezjaw@st.amu.edu.pl>
2020-09-22 02:05:27 +0200

    Adding results to 3rd task

--- zestaw_2.py
+++ zestaw_2.py
@@ -1,4 +1,4 @@
-#importing csv, pandas and sklearn libraries and setting options
+#importing csv files, pandas and sklearn libraries and setting options
 import pandas as pd
 import sklearn.preprocessing as skl
 pd.set_option('display.width',None)
@@ -23,21 +23,23 @@ df.replace(to_replace='500 to 999 employees', value='500', inplace=True)
 df.replace(to_replace='1,000 to 4,999 employees', value='1000', inplace=True)
 df.replace(to_replace='5,000 to 9,999 employees', value='5000', inplace=True)
 df.replace(to_replace='10,000 or more employees', value='10000', inplace=True)
-df.replace(to_replace='No', value='0', inplace=True)
-df.replace(to_replace='Yes, full-time', value='1', inplace=True)
-df.replace(to_replace='Yes, part-time', value='1', inplace=True)
 df.dropna(inplace=True)
 df['OrgSize'] = df['OrgSize'].astype(float)
 df['YearsCode'] = df['YearsCode'].astype(float)
 df['YearsCodePro'] = df['YearsCodePro'].astype(float)
 df['LastHireDate'] = df['LastHireDate'].astype(float)
-df['Student'] = df['Student'].astype(int)
 #seeking for correlations
 df.corr()
 #setting variables
 x1=df['OrgSize']
 x2=df['YearsCodePro']
 y=df['ConvertedComp']
+
+#replacing string values after adding extra columns and converting to int
+df.replace(to_replace='No', value='0', inplace=True)
+df.replace(to_replace='Yes, full-time', value='1', inplace=True)
+df.replace(to_replace='Yes, part-time', value='1', inplace=True)
+df['Student'] = df['Student'].astype(int)
 #creating a binarize lebeled numpy arraies with usage of column UndergradMajor
 label_bin = skl.LabelBinarizer()
 transformed_label_bin = label_bin.fit_transform(df['UndergradMajor'])
@@ -45,3 +47,21 @@ transformed_label_bin = label_bin.fit_transform(df['UndergradMajor'])
 df_label_bin= pd.DataFrame(transformed_label_bin, columns=label_bin.classes_)
 #joining to primary DataFrame
 df = df.join(df_label_bin)
+
+#subsetting new DataFrame with float-typed columns
+df2 = pd.DataFrame(df[['OrgSize', 'YearsCode', "YearsCodePro", "LastHireDate","ConvertedComp"]])
+#droping NA values
+df2.dropna(inplace=True)
+#calculating 1st, 3rd quantile and IQR
+Q1 = df2.quantile(0.25)
+Q3 = df2.quantile(0.75)
+IQR = Q3 - Q1
+#removing the outliers from DataFrame basing on quantiles
+df2_out_quantiles = df2[~((df2 < (Q1 - 1.5 * IQR)) |(df2 > (Q3 + 1.5 * IQR))).any(axis=1)]
+#calculating mean and standard deviation, nextly a Z-score
+df2_mean = df2.mean()
+df2_sd = df2.std()
+z_score=(df2-df2_mean)/df2_sd
+#removing the utliers from DataFrame basing on Z-score
+df2_out_sd = df2[~((z_score > 3) | (z_score < -3)).any(axis=1)]
+
